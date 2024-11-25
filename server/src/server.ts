2eed4cf5733e95403409f0cc9546e4e3496fc3e6
JD Tadlock <jt7903@gmail.com>
2024-11-25 11:31:41 -0500

    test push

--- server/src/server.ts
+++ server/src/server.ts
@@ -37,6 +37,7 @@ connection.once('open', async () => {
   );
 
   if (process.env.PORT) {
+
     const __dirname = path.dirname(new URL(import.meta.url).pathname);
     // Share all files in the client/dist folder with the client-side
     app.use(express.static(path.join(__dirname, '../../client/dist')));
